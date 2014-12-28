fcfca26f151b715b8ddd666348a9b9c22f2a3b7e
Gaurish Rane <megaurishrane@gmail.com>
2014-12-28 14:45:40 +0530

    some change

---
+++ fuel/app/logs/2014/12/28.php
@@ -0,0 +1,8 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-12-28 08:39:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-12-28 08:39:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-12-28 08:39:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-12-28 08:39:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-12-28 08:39:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-12-28 08:39:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
